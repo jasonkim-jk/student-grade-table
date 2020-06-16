33fa8d002994d0e3d8112de661299b1a39f98c24
Jason Kim <thehesedlife@gmail.com>
2020-06-16 14:06:36 -0700

    Requesting Data added App class and some method ajax GET working well

---
+++ components/app.js
@@ -0,0 +1,30 @@
+class App {
+  constructor() {
+    this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess.bind(this);
+  }
+
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {
+        "X-Access-Token": "R1JRk9us",
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError,
+    });
+  }
+
+  start() {
+    this.getGrades();
+  }
+}
