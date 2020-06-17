928c59c69faaeb6d85fb39cda58066416e62441f
Jason Kim <thehesedlife@gmail.com>
2020-06-16 17:15:40 -0700

    Add a GradeForm Component added GradeForm class updated App class to get gradeForm instance

---
+++ components/grade-form.js
@@ -0,0 +1,15 @@
+class GradeForm {
+  constructor(formElement) {
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement = formElement;
+  }
+
+  onSubmit(createGrade) {
+    this.createGrade = createGrade;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("Test: Hi");
+  }
+}
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var studentTable = document.querySelector(".tbody");
 var studentTableHeader = document.querySelector("header");
+var formContainer = document.querySelector(".form-container");
+
 var pageHeader = new PageHeader(studentTableHeader);
 var gradeTable = new GradeTable(studentTable);
-var app = new App(gradeTable, pageHeader);
+var gradeForm = new GradeForm(formContainer);
+
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
