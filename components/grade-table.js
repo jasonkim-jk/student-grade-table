e25542a4d9d4eed66a6d34f4e706d74cbf3c59fe
Jason Kim <thehesedlife@gmail.com>
2020-06-17 01:14:09 -0700

    Enhance the GradeTable Component added a method to update table row with student data from a server added event listener for each delete button linking delete button and passing each student id

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,25 +1,21 @@
 class GradeTable {
-  constructor(tableElement) {
+  constructor(tableElement, noGradesElement) {
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
 
   updateGrades(grades) {
     console.log(grades);
     this.cleanTable();
-    for (var i = 0; i < grades.length; i++) {
-      var tr = document.createElement("tr");
-      var tdName = document.createElement("td");
-      var tdCourse = document.createElement("td");
-      var tdGrade = document.createElement("td");
-      var tdOperation = document.createElement("td");
 
-      tdName.textContent = grades[i].name;
-      tdCourse.textContent = grades[i].course;
-      tdGrade.textContent = grades[i].grade;
-      tdOperation.textContent = "";
-
-      tr.append(tdName, tdCourse, tdGrade, tdOperation);
-      this.tableElement.appendChild(tr);
+    if (grades.length > 0) {
+      this.noGradesElement.classList.add("d-none");
+      for (var i = 0; i < grades.length; i++) {
+        var tableRow = this.renderGradeRow(grades[i], this.deleteGrade);
+        this.tableElement.appendChild(tableRow);
+      }
+    } else {
+      this.noGradesElement.classList.remove("d-none");
     }
   }
 
@@ -27,5 +23,33 @@ class GradeTable {
     while (this.tableElement.firstChild) {
       this.tableElement.removeChild(this.tableElement.lastChild);
     }
+  }
+
+  onDeleteClick(deleteGrade) {
+    this.deleteGrade = deleteGrade;
+  }
+
+  renderGradeRow(data, deleteGrade) {
+    var tr = document.createElement("tr");
+    var tdName = document.createElement("td");
+    var tdCourse = document.createElement("td");
+    var tdGrade = document.createElement("td");
+    var tdOperation = document.createElement("td");
+    var btn = document.createElement("button");
+
+    tdName.textContent = data.name;
+    tdCourse.textContent = data.course;
+    tdGrade.textContent = data.grade;
+    tdOperation.textContent = "";
+    btn.textContent = "DELETE";
+    btn.className = "btn btn-danger";
+
+    tdOperation.appendChild(btn);
+    tr.append(tdName, tdCourse, tdGrade, tdOperation);
+    btn.addEventListener("click", function () {
+      deleteGrade(data.id);
+    });
+
+    return tr;
   }
 }
